d2e13a496ebb17b14abbe7a40e6495283a6d71e0
yejin2329 <kimye232917@gmail.com>
2024-05-03 23:11:22 -0400

    modify file

---
+++ client/src/components/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Login(){
+    return(
+        <div>
+            <h2>Login</h2>
+            <form>
+
+            </form>
+        </div>
+    )
+}
+
+export default Login;
---
+++ client/src/components/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MainPage(){
+    return(
+        <div>
+            <h1>Welcome to the Weather-Based Outfit Recommender</h1>
+            <p>Get your outfit recommendation based on today's weather.</p>
+
+        </div>
+    )
+}
+
+export default MainPage;

badd2bfb83955f49c33503766455b1b80b1cacd7
yejin2329 <kimye232917@gmail.com>
2024-05-03 23:37:11 -0400

    add form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function Login(){
+    const [userId, setUserId]=useState('');
+    const [password, setPassword]=useState('');
+
+    const handleLogin=(e)=>{
+        e.preventDefault();
+        console.log('Logging in with:', userId, password);
+    }
     return(
         <div>
             <h2>Login</h2>
-            <form>
-
+            <form onSubmit={handleLogin}>
+                <div>
+                    <label htmlFor="userId">User ID:</label>
+                    <input type= "text"
+                            id="userId"
+                            value={userId}
+                            onChange={(e)=>setUserId(e.target.value)}
+                            required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Passwored:</label>
+                    <input type="password"
+                            id="password"
+                            value={password}
+                            onChange={(e)=>setPassword(e.target.value)}
+                            required
+                    />
+                </div>
+                <button type="submit">Login</button>
             </form>
         </div>
     )

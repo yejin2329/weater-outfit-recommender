7b4be9a6d5a7c11f514b8b2f8f487cf757a8127c
yejin2329 <kimye232917@gmail.com>
2024-05-04 22:42:00 -0400

    modify backend

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,20 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.post('/login', (req,res)=>{
+  const {userId, password}=req.body;
+  if(authenticate(userId, password)){
+    res.status(200).json({message:"Login successful!"})
+  }else{
+    res.status(401).json({message:"Login failed!"})
+  }
+})
+
+function authenticate(userId, password){
+  //logic
+
+  return true;
+}
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });

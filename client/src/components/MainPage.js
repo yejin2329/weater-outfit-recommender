82a956bc05b9a7c4bfa1811faf19269ac19f587d
yejin2329 <kimye232917@gmail.com>
2024-05-06 22:19:49 -0400

    updates

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function MainPage(){
     return(
         <div>
             <h1>Welcome to the Weather-Based Outfit Recommender</h1>
             <p>Get your outfit recommendation based on today's weather.</p>
-
+            <Link to="/login"><button>Login</button></Link>
+            <p>Don't have an account? <Link to="/register">Click here to register</Link></p>
         </div>
     )
 }

6591ec3a9d53953f61dab1696dbe9e3fe3e5260f
yejin2329 <kimye232917@gmail.com>
2024-05-12 23:09:37 -0400

    add email

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -5,6 +5,7 @@ function Register(){
     const [userId, setUserId]=useState('');
     const [password, setPassword]=useState('');
     const [confirmPassword, setConfirmPassword]=useState('');
+    const [email, setEmail]=useState('');
     const [error, setError]=useState('');
     const [success, setSuccess]=useState('');
 
@@ -24,13 +25,18 @@ function Register(){
             setError("Password do not match!")
             return;
         }
+
+        if(!validateEmail(email)){
+            setError("Invalid email address.")
+            return;
+        }
         try{
             const response=await fetch('http://localhost:5000/register', {
                 method: 'POST',
                 headers:{
                     'Content-type':'application/json'
                 },
-                body:JSON.stringify({userId, password})
+                body:JSON.stringify({userId, password, email})
         })
         const data=await response.json();
         if(response.ok){
@@ -51,12 +57,26 @@ function Register(){
         return password.length>=6 && /[A-Z]/.test(password) &&  /\d/.test(password);
     }
 
+    function validateEmail(email){
+        return /\S+@\S+\.\S+/.test(email); 
+    }
+
     return(
         <div>
             <h2>Register</h2>
             {error && <p style={{ color: 'red' }}>{error}</p>}
             {success && <p style={{color:'green'}}>{success}</p>}
             <form onSubmit={handleRegistration}>
+                 <div>
+                      <label htmlFor="email">Email:</label>
+                       <input type="email"
+                                id="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required 
+                        />
+                </div>
+
                 <div>
                     <label htmlFor="userId">User ID:</label>
                     <input type="text"

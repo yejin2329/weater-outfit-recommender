fb0a2515d00f768e195a62f15a087b2c11904bf1
yejin2329 <kimye232917@gmail.com>
2024-05-12 23:08:36 -0400

    add email

--- backend/models/User.js
+++ backend/models/User.js
@@ -10,6 +10,11 @@ const userSchema=new mongoose.Schema({
     password:{
         type:String,
         required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
     }
 })
 

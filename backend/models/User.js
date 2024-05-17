af2df83ae6b73f6aafd36f2bd9667a1221f8e73f
yejin2329 <kimye232917@gmail.com>
2024-05-16 23:39:19 -0400

    updates

--- backend/models/User.js
+++ backend/models/User.js
@@ -15,6 +15,11 @@ const userSchema=new mongoose.Schema({
         type:String,
         required:true,
         unique:true
+    },
+    clothingPreferences: {
+        cold: { type: String },
+        hot: { type: String },
+        rainy: { type: String }
     }
 })
 

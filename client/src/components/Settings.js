21234b50d9698f3e97cd8ed40f12176df03a1e0a
yejin2329 <kimye232917@gmail.com>
2024-05-17 23:56:45 -0400

    add new file

---
+++ client/src/components/Settings.js
@@ -0,0 +1,14 @@
+// Settings.js
+import React from 'react';
+import ClothingPreferences from './ClothingPreferences';
+
+function Settings() {
+    return (
+        <div>
+            <h1>Settings</h1>
+            <ClothingPreferences />
+        </div>
+    );
+}
+
+export default Settings;

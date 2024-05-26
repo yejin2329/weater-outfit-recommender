7968834b05aa001a5fc29740bd4d279de03b739b
yejin2329 <kimye232917@gmail.com>
2024-05-26 18:13:01 -0400

    updates for weather data

--- backend/server.js
+++ backend/server.js
@@ -43,6 +43,20 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+//function for weather API
+async function fetchWeather(lat,lon){
+  const apiKey='';
+  const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  try{
+    const response=await fetch(url);
+    const data=await response.json();
+    return data;
+  }catch(error){
+    console.error('Failed to fetch weather data:',error);
+    throw error;
+  }
+}
+
 //login
 app.post('/login', async(req,res)=>{
   const {username, password}=req.body;

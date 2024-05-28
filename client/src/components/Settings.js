ff72bbd19c4a46cff4197dd497ebffb6320f5f75
yejin2329 <kimye232917@gmail.com>
2024-05-27 23:07:39 -0400

    add location dropdown

--- client/src/components/Settings.js
+++ client/src/components/Settings.js
@@ -3,6 +3,16 @@ import React from 'react';
 import ClothingPreferences from './ClothingPreferences';
 
 function Settings() {
+    
+const canadaCities = [
+    { name: "Toronto", lat: 43.651070, lon: -79.347015 },
+    { name: "Vancouver", lat: 49.282730, lon: -123.120735 },
+    { name: "Montreal", lat: 45.501690, lon: -73.567253 },
+    { name: "Calgary", lat: 51.044734, lon: -114.071883 },
+    { name: "Ottawa", lat: 45.421530, lon: -75.697193 },
+    
+  ];
+  
     return (
         <div>
             <div className="setting-container">

5b94ec5c76fedee308fa2262dd030cd84c8d69ff
yejin2329 <kimye232917@gmail.com>
2024-05-27 23:32:06 -0400

    updates UI

--- client/src/components/Settings.js
+++ client/src/components/Settings.js
@@ -1,23 +1,43 @@
 // Settings.js
-import React from 'react';
+import React, {useState} from 'react';
 import ClothingPreferences from './ClothingPreferences';
 
 function Settings() {
-    
-const canadaCities = [
-    { name: "Toronto", lat: 43.651070, lon: -79.347015 },
-    { name: "Vancouver", lat: 49.282730, lon: -123.120735 },
-    { name: "Montreal", lat: 45.501690, lon: -73.567253 },
-    { name: "Calgary", lat: 51.044734, lon: -114.071883 },
-    { name: "Ottawa", lat: 45.421530, lon: -75.697193 },
-    
-  ];
+    const[selectedCity, setSelectedCity]=useState('');
+    const canadaCities = [
+        { name: "Toronto", lat: 43.651070, lon: -79.347015 },
+        { name: "Vancouver", lat: 49.282730, lon: -123.120735 },
+        { name: "Montreal", lat: 45.501690, lon: -73.567253 },
+        { name: "Calgary", lat: 51.044734, lon: -114.071883 },
+        { name: "Ottawa", lat: 45.421530, lon: -75.697193 },
+        
+      ];
+
+    const handleLocationChange=(event)=>{
+        const city=canadaCities.find(city=>city.name===event.target.value)
+        setSelectedCity(event.target.value);
+        updateLocation(city);
+    }    
+
+    const updateLocation=async(city)=>{
+        console.log("Updating location to:", city)
+    }
+
   
     return (
         <div>
             <div className="setting-container">
             <h1>Settings</h1>
             <ClothingPreferences />
+            <div>
+                <label htmlFor="city-select">Choos your city: </label>
+                <select id="city-select" value={selectedCity} onChange={handleLocationChange}>
+                    <option value="">Select a city</option>
+                    {canadaCities.map(city=>(
+                        <option key={city.name} value={city.name}>{city.name}</option>
+                    ))}
+                </select>
+            </div>
             </div>
         </div>
     );

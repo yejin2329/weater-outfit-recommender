16be01ff81a9ce0ea56ec15c0ab11358cff8bd19
yejin2329 <kimye232917@gmail.com>
2024-06-12 23:19:26 -0400

    modified

--- client/src/components/Settings.js
+++ client/src/components/Settings.js
@@ -86,11 +86,12 @@ function Settings() {
         setLoading(true);
         try{
             const response=await fetch(`http://localhost:5000/api/users/preferences/${user._id}`)
+            const data=await response.json();
             if(!response.ok){
                 const errorText=await response.text();
                 throw new Error('Failed to fetch preferences: '+errorText)
             }
-            const data=await response.json();
+           
             //debug
             console.log("Fetched preferences:", data)
 

3bc336d6b4405b4bff5cd140ac7e15fa22905dbc
yejin2329 <kimye232917@gmail.com>
2024-06-18 22:14:23 -0400

    add useEffect

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -6,7 +6,14 @@ export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')));
-
+  
+  useEffect(()=>{
+    const storedUser=localStorage.getItem('user');
+    if(storedUser){
+      setUser(JSON.parse(storedUser));
+    }
+  },[]);
+  
   const login = (userData) => {
     console.log("Logging in user, userData received: ", userData)
     setUser(userData);

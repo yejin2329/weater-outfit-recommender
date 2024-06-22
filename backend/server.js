117aff891b5d037c2086e8ffa3d466a69c1d2646
yejin2329 <kimye232917@gmail.com>
2024-06-22 18:11:00 -0400

    modified

--- backend/server.js
+++ backend/server.js
@@ -205,13 +205,13 @@ app.post('/api/users/preferences', async(req,res)=>{
   console.log(`Updating user Id ${_id} with preferences: ${JSON.stringify(preferences)}, sensitivity: ${JSON.stringify(sensitivity)}, city: ${city}`)
   
   const updateData={
-    'clothingPreferences.cold':preferences.cold,
-    'clothingPreferences.hot':preferences.hot,
-    'clothingPreferences.rainy':preferences.rainy,
-    'weatherSensitivity.coldSensitive':sensitivity.coldSensitivity,
-    'weatherSensitivity.heatSensitive':sensitivity.heatSensitivity,
-    'weatherSensitivity.windSensitive':sensitivity.windSensitivity,
-    'defaultCity':city
+    'clothingPreferences.cold':req.body.preferences.cold,
+    'clothingPreferences.hot':req.body.preferences.hot,
+    'clothingPreferences.rainy':req.body.preferences.rainy,
+    'weatherSensitivity.coldSensitive':req.body.sensitivity.coldSensitivity,
+    'weatherSensitivity.heatSensitive':req.body.sensitivity.heatSensitivity,
+    'weatherSensitivity.windSensitive':req.body.sensitivity.windSensitivity,
+    'defaultCity':req.body.city
   }
 
   //console.log("Update Data to be applied:", updateData)

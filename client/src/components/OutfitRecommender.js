b408efbfaaa85390c4864e807d2b8be9343e5cd9
yejin2329 <kimye232917@gmail.com>
2024-06-26 12:53:05 -0400

    updates

--- client/src/components/OutfitRecommender.js
+++ client/src/components/OutfitRecommender.js
@@ -1,5 +1,16 @@
-function OutfitRecommener(){
+//function OutfitRecommener(){
+    
+const recommendOutfit=(weather, preferences, sensitivity)=>{
+        let recommendations=[];
+        if(weather.temperature<5){
+            recommendations.push('Thermal Jacket')
+            if(sensitivity.coldSensitive){
+                recommendations.push('Scarf', 'Gloves')
+            }
+        }else if(weather.temperature>=5 && weather.temperature<20){
+            recommendations.push('Light Jacket', 'Long Sleeve Shirt')
+        }
+    }
 
-}
 
-export default OutfitRecommener;
+//export default recommendations;

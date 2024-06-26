a9ae5e1346f9951fd02b26bdb6d118b426a1749a
yejin2329 <kimye232917@gmail.com>
2024-06-26 13:56:15 -0400

    import recommendOutfit

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 import {useAuth} from '../contexts/AuthContext';
+import recommendOutfit from './OutfitRecommender';
 
 
 function MainPage(){

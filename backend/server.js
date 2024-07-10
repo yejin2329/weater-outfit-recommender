cda4085ec632f21e199ab2dce4af0f1113d9ef7f
yejin2329 <kimye232917@gmail.com>
2024-07-09 23:23:44 -0400

    add debug

--- backend/server.js
+++ backend/server.js
@@ -47,6 +47,10 @@ app.get('/', (req, res) => {
 async function fetchWeather(lat,lon){
   const{default:fetch}=await import('node-fetch')
   const apiKey=process.env.OPENWEATHER_API_KEY;
+  if(!apiKey){
+    console.log('API Key is missing.');
+    throw new Error('API Key for OpenWeather is undefined')
+  }
   const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   try{
     const response=await fetch(url);

50543097d41b4830c690291369dcd27af1fd3d1c
yejin2329 <kimye232917@gmail.com>
2024-07-13 23:58:07 -0400

    modified

--- backend/models/testUserOperations.js
+++ backend/models/testUserOperations.js
@@ -24,7 +24,6 @@ async function createUser() {
         console.log('User created:', newUser);
     } catch (error) {
         console.error('Error creating user:', error.message);
-        //console.log(error);
     }
 }
 
